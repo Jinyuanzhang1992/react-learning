16f9445ac2cf762c2a9d717db17237f2b6249c90
Jinyuan Zhang <jinyuanzhang1992@hotmail.com>
2024-05-30 09:53:25 +0930

    Summary: learning JSX basic rules Description: write the basic rules step by step Resolve: CP-10

--- src/my-clock/index.js
+++ src/my-clock/index.js
@@ -1,6 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function MyClock(props) {
+  const pRef = useRef(null);
+
   const [time, setTime] = useState(new Date().toLocaleString());
   // 钩子函数 time是变量，setTime是函数
 
@@ -20,6 +22,18 @@ function MyClock(props) {
       <h1>React Clock</h1>
       <h2>{time}</h2>
       {/* {}jsx提供的模板语法，可以调用变量 */}
+      <div ref={pRef}>
+        <h3>1 + 1 = {1 + 1}</h3>
+        <h3>Min value between 1 and 2 is {Math.min(1, 2)}</h3>
+      </div>
+      <button
+        onClick={(e) => {
+          console.log(pRef.current.innerText);
+          alert(pRef.current.innerText);
+        }}
+      >
+        Submit
+      </button>
     </div>
   );
 }
